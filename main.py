9f416745128e84c8647d5b48bc54faaf4820f4f2
khatir <sebastienkhatir@gmail.com>
2026-06-02 16:51:40 +0200

    Update main for category scraping

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from extract import extract_data
+from extract import extract_book_data, get_books_urls
 from transform import transform_data
 from load import load_data
 
 
 def main():
-    books = extract_data()
+
+    category_url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+
+    book_urls = get_books_urls(category_url)
+
+    books = []
+
+    for url in book_urls:
+        books.append(extract_book_data(url))
+
     books = transform_data(books)
     load_data(books)
 
